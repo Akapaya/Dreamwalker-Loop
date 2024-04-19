d22ffbf0e925f3f70e8b178a6f1a62a3264985bb
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-04-18 21:26:38 -0300

    Materials Update
    Materials Update

---
+++ Assets/Scripts/AnomalyActions/RotateObjects.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjects : MonoBehaviour, IAnomaly
+{
+    [SerializeField] private List<GameObject> _objectsToDeactivesList;
+
+    public void OnActivateAnomaly()
+    {
+        foreach (GameObject obj in _objectsToDeactivesList)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public void OnDeactivateAnomaly()
+    {
+        foreach (GameObject obj in _objectsToDeactivesList)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
